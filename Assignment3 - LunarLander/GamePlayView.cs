ec216117e3ad18c51e15a279fe04d39041a5f044
Tyler Johnston <johnstontyler@protonmail.com>
2024-03-08 08:11:09 -0700

    more refactoring by making terrain generator class

--- Assignment3 - LunarLander/GamePlayView.cs
+++ Assignment3 - LunarLander/GamePlayView.cs
@@ -7,6 +7,10 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Random;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
 
 namespace CS5410
 
@@ -16,10 +20,6 @@ namespace CS5410
         private SpriteFont m_font;
         private Texture2D m_background;
         private Texture2D m_lunarLander;
-        private BasicEffect m_effect;
-        private VertexPositionColor[] m_vertsTris;
-        private int[] m_indexTris;
-        private VertexPositionColor[] m_outline;
         private Song m_music;
         private SoundEffect m_explosion;
         private SoundEffect m_landed;
@@ -27,8 +27,7 @@ namespace CS5410
         private SoundEffectInstance thrustersInstance;
         private RandomMisc randomMisc = new RandomMisc();
         private LunarLander lunarLander;
-        private List<SafeZone> safeZones;
-        private List<Vector2> terrain;
+        private TerrainGenerator terrainGenerator;
         private float m_landerScale;
         public enum GameStatus
         {
@@ -63,133 +62,16 @@ namespace CS5410
 
             thrustersInstance = m_thrusters.CreateInstance();
             thrustersInstance.IsLooped = true;
+
             MediaPlayer.Play(m_music);
+            MediaPlayer.IsRepeating = true;
 
-            InitializeTerrain();
-            GenerateTerrainOutline();
-            FillTerrain();
+            terrainGenerator = new TerrainGenerator(minY, maxY, level, randomMisc, m_graphics);
+            terrainGenerator.InitializeTerrain();
+            terrainGenerator.GenerateTerrainOutline();
+            terrainGenerator.FillTerrain();
+
             InitializeLunarLander();
-        }
-
-        private void InitializeTerrain()
-        {
-            Vector2 startPoint = new Vector2(0, minY + (float)randomMisc.nextDouble() * (maxY - minY));
-            Vector2 endPoint = new Vector2(1, minY + (float)randomMisc.nextDouble() * (maxY - minY));
-            int iterations = 7;
-            float roughness = 4.0f;
-            float safeZoneDistance = .08f;
-            safeZones = new List<SafeZone>();
-            if (level == 1)
-            {
-                AddSafeZones(2, safeZoneDistance);
-            }
-            else
-            {
-                AddSafeZones(1, safeZoneDistance - .02f);
-            }
-            terrain = RandomMidpointDisplacement(startPoint, endPoint, roughness, iterations);
-            terrain.Insert(0, startPoint);
-            terrain.Add(endPoint);
-        }
-
-        private void GenerateTerrainOutline()
-        {
-            int terrainCount = terrain.Count;
-            int scaleX = m_graphics.PreferredBackBufferWidth;
-            int scaleY = m_graphics.PreferredBackBufferHeight;
-            m_outline = new VertexPositionColor[(terrainCount - 1) * 2];
-
-            for (int i = 0; i < terrainCount - 1; i++)
-            {
-                int index = i * 2;
-                Vector2 start = terrain[i];
-                Vector2 end = terrain[i + 1];
-
-                m_outline[index] = new VertexPositionColor(new Vector3(start.X * scaleX, scaleY - (start.Y * scaleY), 0), Color.Black);
-                m_outline[index + 1] = new VertexPositionColor(new Vector3(end.X * scaleX, scaleY - (end.Y * scaleY), 0), Color.Black);
-            }
-        }
-
-        private void FillTerrain()
-        {
-            m_graphics.GraphicsDevice.RasterizerState = new RasterizerState
-            {
-                FillMode = FillMode.Solid,
-                CullMode = CullMode.CullClockwiseFace,
-                MultiSampleAntiAlias = true,
-            };
-
-            m_effect = new BasicEffect(m_graphics.GraphicsDevice)
-            {
-                VertexColorEnabled = true,
-                View = Matrix.CreateLookAt(new Vector3(0, 0, 1), Vector3.Zero, Vector3.Up),
-
-                Projection = Matrix.CreateOrthographicOffCenter(
-                    0, m_graphics.GraphicsDevice.Viewport.Width,
-                    m_graphics.GraphicsDevice.Viewport.Height, 0,   // doing this to get it to match the default of upper left of (0, 0)
-                    0.1f, 2)
-            };
-
-            int scaleX = m_graphics.PreferredBackBufferWidth;
-            int scaleY = m_graphics.PreferredBackBufferHeight;
-            int bottomHeight = m_graphics.PreferredBackBufferHeight;
-
-            int terrainCount = terrain.Count;
-            int vertexCount = (terrainCount - 1) * 6;
-            m_vertsTris = new VertexPositionColor[vertexCount];
-            m_indexTris = new int[vertexCount];
-
-            for (int i = 0, v = 0; i < terrainCount - 1; i++, v += 6)
-            {
-                Vector3 bottomLeft = new Vector3(terrain[i].X * scaleX, bottomHeight, 0);
-                Vector3 topLeft = new Vector3(terrain[i].X * scaleX, scaleY - (terrain[i].Y * scaleY), 0);
-                Vector3 topRight = new Vector3(terrain[i + 1].X * scaleX, scaleY - (terrain[i + 1].Y * scaleY), 0);
-                Vector3 bottomRight = new Vector3(terrain[i + 1].X * scaleX, bottomHeight, 0);
-
-                m_vertsTris[v].Position = bottomLeft;
-                m_vertsTris[v + 1].Position = topLeft;
-                m_vertsTris[v + 2].Position = topRight;
-
-                m_vertsTris[v + 3].Position = bottomLeft;
-                m_vertsTris[v + 4].Position = topRight;
-                m_vertsTris[v + 5].Position = bottomRight;
-
-                // apply colors
-                for (int j = 0; j < 6; j++)
-                {
-                    m_vertsTris[v + j].Color = Color.LightGray; 
-                }
-
-                // set up the indices
-                m_indexTris[v] = v;
-                m_indexTris[v + 1] = v + 1;
-                m_indexTris[v + 2] = v + 2;
-                m_indexTris[v + 3] = v + 3;
-                m_indexTris[v + 4] = v + 4;
-                m_indexTris[v + 5] = v + 5;
-            }
-        }
-
-        private void AddSafeZones(int count, float distance)
-        {
-            for (int i=0; i < count; i++)
-            {
-                Vector2 startPoint = new Vector2((float)randomMisc.nextDoubleInRange(.15, .85), minY + (float)randomMisc.nextDouble() * (maxY - minY));
-                Vector2 endPoint = new Vector2(startPoint.X + distance, startPoint.Y);
-                safeZones.Add(new SafeZone(startPoint, endPoint));
-            }
-        }
-
-        private SafeZone getSafeZone(Vector2 point)
-        {
-            foreach (var zone in safeZones)
-            {
-                if (point.X >= zone.Start.X && point.X <= zone.End.X)
-                {
-                    return zone;
-                }
-            }
-            return null;
         }
 
         private void InitializeLunarLander()
@@ -211,55 +93,13 @@ namespace CS5410
 
             // generate a random rotation. Assuming full rotation is from 0 to 2*PI radians
             float randomRotation = (float)(randomMisc.nextDouble() * Math.PI * 2);
-            
-            lunarLander = new LunarLander(new Vector2(landerX, landerYPositionMargin - (m_lunarLander.Height * m_landerScale)), randomRotation, m_landerScale, 20);
+
+            const float BaseFuel = 20;
+            const float a = 0.15f;
+            float adjustedFuel = BaseFuel / (1 + a * (level - 1));
+    
+            lunarLander = new LunarLander(new Vector2(landerX, landerYPositionMargin - (m_lunarLander.Height * m_landerScale)), randomRotation, m_landerScale, adjustedFuel);
         }
-
-        private List<Vector2> RandomMidpointDisplacement(Vector2 start, Vector2 end, float roughness, int iterations)
-        {
-            if (iterations == 0)
-            {
-                return new List<Vector2>();
-            }
-
-            float midX = (start.X + end.X) / 2;
-            float midY = (start.Y + end.Y) / 2;
-            SafeZone mySafeZone = getSafeZone(new Vector2(midX, midY));
-            if (mySafeZone == null)
-            {
-                float displacement = roughness * (float)randomMisc.nextGaussian(0, .375) * Math.Abs(end.X - start.X);
-                midY += displacement;
-                midY = Math.Min(Math.Max(midY, minY), maxY);
-            }
-            else
-            {
-                midY = mySafeZone.Start.Y;
-            }
-            Vector2 midPoint = new Vector2(midX, midY);
-
-            List<Vector2> left = RandomMidpointDisplacement(start, midPoint, roughness / 1.1f, iterations - 1);
-            List<Vector2> right = RandomMidpointDisplacement(midPoint, end, roughness / 1.1f, iterations - 1);
-
-            left.Add(midPoint);
-            left.AddRange(right);
-            return left;
-        }
-
-        private void resetGameState()
-        {
-            level += 1;
-            gameStatus = GameStatus.Playing;
-            countdown = 3;
-            lastUpdateTime = 0;
-            explosionPlayed = false;
-            landedPlayed = false;
-
-            InitializeTerrain();
-            GenerateTerrainOutline();
-            FillTerrain();
-            InitializeLunarLander();
-        }
-
 
         public override GameStateEnum processInput(GameTime gameTime)
         {
@@ -363,24 +203,42 @@ namespace CS5410
             m_spriteBatch.Begin();
             m_spriteBatch.Draw(m_background, new Rectangle(0, 0, m_graphics.GraphicsDevice.Viewport.Width, m_graphics.GraphicsDevice.Viewport.Height), Color.White);
             m_spriteBatch.End();
-            foreach (EffectPass pass in m_effect.CurrentTechnique.Passes)
+
+            foreach (EffectPass pass in terrainGenerator.Effect.CurrentTechnique.Passes)
             {
                 pass.Apply();
 
                 m_graphics.GraphicsDevice.DrawUserIndexedPrimitives(
                     PrimitiveType.TriangleList, 
-                    m_vertsTris, 0, m_vertsTris.Length, 
-                    m_indexTris, 0, m_indexTris.Length / 3);
+                    terrainGenerator.Vertices, 0, terrainGenerator.Vertices.Length, 
+                    terrainGenerator.Indices, 0, terrainGenerator.Indices.Length / 3);
 
                 m_graphics.GraphicsDevice.DrawUserPrimitives(
-                    PrimitiveType.LineList, m_outline, 0, m_outline.Length / 2);
+                    PrimitiveType.LineList, terrainGenerator.Outline, 0, terrainGenerator.Outline.Length / 2);
             }
+
             m_spriteBatch.Begin();
             renderHUD();
             renderLunarLander();
             renderCrashed();
             renderLanded();
             m_spriteBatch.End();
+        }
+
+        private void resetGameState()
+        {
+            level += 1;
+            gameStatus = GameStatus.Playing;
+            countdown = 3;
+            lastUpdateTime = 0;
+            explosionPlayed = false;
+            landedPlayed = false;
+
+            terrainGenerator = new TerrainGenerator(minY, maxY, level, randomMisc, m_graphics);
+            terrainGenerator.InitializeTerrain();
+            terrainGenerator.GenerateTerrainOutline();
+            terrainGenerator.FillTerrain();
+            InitializeLunarLander();
         }
 
         private void updatePlayingState(GameTime gameTime)
@@ -470,6 +328,7 @@ namespace CS5410
         {
             int scaleX = m_graphics.PreferredBackBufferWidth;
             int scaleY = m_graphics.PreferredBackBufferHeight;
+            var terrain = terrainGenerator.Terrain;
 
             for (int i = 0; i < terrain.Count - 1; i++)
             {
